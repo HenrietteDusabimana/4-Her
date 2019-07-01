7c0c0e8f8163f34c84f2478f0013688e3fe32f55
Henriette Dusabimana <Henriette@Henriettes-MacBook-Air.local>
2019-07-01 10:46:28 +0200

    officially added th eproject on git hub

---
+++ js/pagescript.js
@@ -0,0 +1,14 @@
+<script type="text/javascript">
+$(document).ready(function(){
+  $('.ourPrograms').slick({
+    settingname: setting-value
+  })
+});
+</script>
+$('.ourPrograms').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+          
